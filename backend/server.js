05cdd8b4b05e01c18f859d278000aab9a830a2ea
unknown <santimallalakshmidhar@gmail.com>
2025-05-07 20:40:28 +0530

    added endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 
 import authRoutes from "./controllers/authController.js";
+<<<<<<< HEAD
 import workerProfileRoutes from "./controllers/workerController.js"; 
 import jobRoutes from './controllers/jobController.js';
+=======
+import jobRoutes from "./controllers/jobController.js";
+>>>>>>> 348ed8e51fc6b2275a5ab58d3af10769ecbe72f9
 
 
 dotenv.config();
@@ -19,8 +23,12 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+<<<<<<< HEAD
 app.use("/api/worker-profile", workerProfileRoutes); 
 app.use('/api/jobs', jobRoutes);
+=======
+app.use("/api/jobs", jobRoutes);
+>>>>>>> 348ed8e51fc6b2275a5ab58d3af10769ecbe72f9
 
 app.get("/", (req, res) => {
   res.send("API is running...");
